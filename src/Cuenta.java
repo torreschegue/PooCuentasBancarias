58f78ca8b6dc2c0bccd8fa15d1ca2d2a71a13958
Cristian Torres <torreschegue@gmail.com>
2022-03-13 18:03:26 -0600

    Desarrollo de clase Cuenta

--- src/Cuenta.java
+++ src/Cuenta.java
@@ -1,4 +1,28 @@
 
 public class Cuenta {
+	private int numeroCuenta; 
+	private double saldo;
+	
+	public Cuenta(int numeroCuenta, double saldo) {
+		this.numeroCuenta = numeroCuenta;
+		this.saldo = saldo;
+	}
 
+	public int getNumeroCuenta() {
+		return numeroCuenta;
+	}
+
+	public double getSaldo() {
+		return saldo;
+	}
+	
+	public void ingresarDinero(double Cantidad) {
+		saldo += Cantidad;
+	}
+	
+	public void retirarDinero(double Cantidad) {
+		saldo -= Cantidad;
+	}
+	
+	
 }
